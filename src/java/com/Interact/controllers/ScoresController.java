b9bc0fc50b1ca499a197129b176b134e2d3fd33c
akhilg96 <akhilg96@vt.edu>
2017-11-11 22:04:16 -0500

    Added class statistics and removed footer

--- src/java/com/Interact/controllers/ScoresController.java
+++ src/java/com/Interact/controllers/ScoresController.java
@@ -5,9 +5,10 @@
 package com.Interact.controllers;
 
 import com.Interact.Entities.UserAnswers;
-import com.Interact.Entities.Users;
 import com.Interact.FacadeBeans.UserAnswersFacade;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -29,8 +30,13 @@ public class ScoresController implements Serializable {
     private com.Interact.FacadeBeans.SessionsFacade sessionsFacade;
 
     private List<UserAnswers> sessionAnswers = null;
+    private List<Integer> dummyList = null; // for class stats
 
     private int totalPoints;
+    private String low;
+    private String high;
+    private String avg;
+    private String median;
 
     public ScoresController() {
 
@@ -42,7 +48,43 @@ public class ScoresController implements Serializable {
                 get("sessId");
         this.setSessionAnswers(getFacade().findBySession(session_id));
         this.setTotalPoints(sessionsFacade.findById(session_id).getQuestionsCollection().size());
+        prepareSessionStats();
+        // To display 1 row in class statistics table
+        this.dummyList = new ArrayList<>();
+        dummyList.add(1);
+        
         return "StudentScores.xhtml?faces-redirect=true";
+    }
+
+    private void prepareSessionStats() {
+        int[] scores = new int[this.getSessionAnswers().size()];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        double avg = 0.0;
+        for (int k = 0; k < getSessionAnswers().size(); k++) {
+            scores[k] = Integer.parseInt(sessionAnswers.get(k).getGrade());
+            min = (scores[k] < min) ? scores[k] : min;
+            max = (scores[k] > max) ? scores[k] : max;
+            avg += scores[k] / (this.sessionAnswers.size() * 1.0);
+        }
+        this.setLow(convertToPercent(min * 1.0));
+        this.setHigh(convertToPercent(max * 1.0));
+        this.setAvg(convertToPercent(avg));
+
+        double med = 0.0; // median
+        if (scores.length > 1) {
+            Arrays.sort(scores);
+            int middle = scores.length / 2;
+            if (scores.length % 2 == 1) {
+                med = scores[middle] * 1.0;
+            } else {
+                med = (scores[middle - 1] + scores[middle]) / 2.0;
+            }
+        } else {
+            med = scores[0];
+        }
+
+        this.setMedian(convertToPercent(med));
     }
 
     public List<UserAnswers> getSessionAnswers() {
@@ -66,6 +108,50 @@ public class ScoresController implements Serializable {
     }
 
     public String calculatePercent(String score) {
-        return Math.round((Double.parseDouble(score) / this.getTotalPoints()) * 100) + "%";
+        return convertToPercent(Integer.parseInt(score) * 1.0);
+    }
+
+    private String convertToPercent(double score) {
+        return Math.round((score / this.getTotalPoints()) * 100) + "%";
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public String getAvg() {
+        return avg;
+    }
+
+    public void setAvg(String avg) {
+        this.avg = avg;
+    }
+
+    public String getMedian() {
+        return median;
+    }
+
+    public void setMedian(String median) {
+        this.median = median;
+    }
+
+    public List<Integer> getDummyList() {
+        return dummyList;
+    }
+
+    public void setDummyList(List<Integer> dummyList) {
+        this.dummyList = dummyList;
     }
 }
